ad137374af15c096cfa7f7c4f76c9da36fba1a74
Max Pajewski <max.pajewski@icloud.com>
2022-07-02 16:45:59 -0500

    Add prompt to ask user to change grid size

--- javascript.js
+++ javascript.js
@@ -1,13 +1,30 @@
-const squares = Array.from(document.getElementsByClassName('gridsquare'));
+addGrid();
+createGrid(16);
+createBrush();
 
-squares.forEach(gridsquare => {
-    gridsquare.addEventListener('mouseenter', function(e) {
-        e.target.style.backgroundColor = "aqua";
-        console.log(e);
+function createBrush() {
+    const squares = Array.from(document.getElementsByClassName('gridsquare'));
+    squares.forEach(gridsquare => {
+        gridsquare.addEventListener('mouseenter', function(e) {
+            e.target.style.backgroundColor = '#1F2937';
+        })
+    })
+}
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if(button.id == 'clear') {
+            clearGrid();
+        }
+        else if(button.id == 'rainbow') {
+
+        }
+        else if(button.id == 'change-size') {
+            changeSize();
+        }
     })
 })
-
-createGrid(20);
 
 
 function createGrid(size) {
@@ -25,7 +42,7 @@ function createGrid(size) {
 }
 
 function changeHeight(size) {
-    let heightWidth = 960 / size;
+    let heightWidth = 750 / size;
     let changeRow = document.getElementsByClassName('row');
     for(let i = 0; i < changeRow.length; i++) {
         changeRow[i].style.height = heightWidth + 'px'; 
@@ -35,6 +52,25 @@ function changeHeight(size) {
     for(let j = 0; j < changeCell.length; j++) {
         changeCell[j].style.height = heightWidth + 'px'; 
     }
+}
+
+function changeSize() {
+    let size = prompt('Enter the size grid you would like: (Max 100)');
+    let elem = document.getElementById('whatever');
+    elem.remove();
+    console.log('removed grid');
+    addGrid();
+    console.log('added new grid');
+    createGrid(size);
+    createBrush();
+}
+
+function addGrid() {
+    const addGrid = document.querySelector('.buttons-and-grid');
+    const grid = document.createElement('div');
+    grid.classList.add('grid-container');
+    grid.setAttribute('id', 'whatever');
+    addGrid.appendChild(grid);
 }
 
 
